a3293404cc8bdc932c75ed649be51cd0acdd1e41
Oswaldo Viloria <oswaldovil42@gmail.com>
2023-02-16 16:22:20 +0100

    add function that adds element in faketable2 table and adding comments

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 //    response.send(random)
 //})
 
+//function to see all the elements of the Faketable
 const getAllObject = 'select * from faketable;' 
 app.get('/names', function(req, res) {
   pool.query(getAllObject,(error, result) => {
@@ -26,12 +27,38 @@ app.get('/names', function(req, res) {
   })
 })
 
+//function to see all the elements of the Faketable2
 const getAllObjectFaketable2 = 'select * from faketable2;' 
 app.get('/faketable2', function(req, res) {
   pool.query(getAllObjectFaketable2,(error, result) => {
     res.json(result.rows)
   })
 })
+
+//function to add items to table FakeTable2
+//TODO Validate name is Unique 
+const addObjectToFaketable2 = 'insert into faketable2 (name, title, description, address, city, country, date_event, hour_event, links) values($1,$2,$3,$4,$5,$6,$7,$8,$9);'
+app.post('/faketable2', function(req, res){
+  let faketable2Name = req.body.name
+  let faketable2Title = req.body.title
+  let faketable2Description = req.body.description
+  let faketable2Address = req.body.address
+  let faketable2City = req.body.city
+  let faketable2Country = req.body.country
+  let faketable2DateEvent = req.body.date_event
+  let faketable2HourEvent = req.body.hour_event
+  let faketable2Links = req.body.links
+  let faketable2Array = [faketable2Name,faketable2Title,faketable2Description,faketable2Address,faketable2City,faketable2Country,faketable2DateEvent,faketable2HourEvent,faketable2Links]
+  pool.query(addObjectToFaketable2, faketable2Array, (error, result) => {
+    if (error) {
+      console.error(error);
+      res.send('error adding information')
+    } else {
+      res.send('added information')
+    }
+  })
+})
+
 
 let port = 5000;
 app.listen( port, function () {
